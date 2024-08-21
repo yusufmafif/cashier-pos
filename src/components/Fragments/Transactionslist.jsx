a8901582ac74e25338858297e2eea352b7863ab4
yusufmafif <yusufmafif@gmail.com>
2024-08-21 09:31:55 +0700

    commit

--- src/components/Fragments/Transactionslist.jsx
+++ src/components/Fragments/Transactionslist.jsx
@@ -3,7 +3,7 @@ import Layout from '../../pages/Layout'
 import axios from 'axios'
 import { Link, Navigate } from 'react-router-dom'
 import Cookies from 'js-cookie';
-import Swal from 'sweetalert2/dist/sweetalert2.js';
+import Swal from "sweetalert2";
 
 
 export const Transactionslist = () => {
